cadd55ee41844ec469f2bd7e1672b628115268f8
Rentfan <rentfann@gmail.com>
2020-03-30 02:39:06 +0300

    Add files via upload

---
+++ src/commands/currencyCommands/rich.js
@@ -0,0 +1,46 @@
+const { GenericCommand } = require('../../models/')
+
+module.exports = new GenericCommand(
+  async ({Memer, msg, addCD}) => {
+    let args = msg.args.args
+    await addCD()
+    const emojis = [':first_place:', ':second_place:', ':third_place:']
+    let stats = await Memer.db.getStats()
+    if (stats.clusters[stats.clusters.length - 1].uptime === 0) {
+      return 'bot is still loading, hold ur horses damn'
+    }
+    if (args && args[0] === '-all') {
+      const bigmeme = (id) => new Promise(resolve => {
+        setTimeout(() => resolve({ id }), 1000)
+        Memer.ipc.fetchUser(id)
+          .then(resolve) // this is intentional and also stupid but still intentional
+      })
+      let pls = await Memer.db.topCoins()
+      pls = await Promise.all(pls.map(async g => Object.assign(await bigmeme(g.id), { coin: g.coin })))
+      return {
+        title: 'Top 15 Global Richest Users',
+        description: pls.map((u, i) => `${emojis[i] || '👏'} ${u.coin.toLocaleString()} - ${u.username ? u.username + '#' + u.discriminator : (Memer.db.removeUser(u.id) && 'LOL WHO DIS')}`).join('\n'),
+        footer: { text: `Global Leaderboard` }
+      }
+    } else {
+      let pls = []
+      let members = msg.channel.guild.members
+      for (const ok of members) {
+        let ding = await Memer.db.getUser(ok[0])
+        pls.push(ding)
+      }
+      pls = pls.filter(u => u.coin > 0)
+      pls = pls.sort((a, b) => b.coin - a.coin).slice(0, 5)
+      pls = await Promise.all(pls.map(async g => Object.assign(await Memer.ipc.fetchUser(g.id), { coin: g.coin })))
+      return {
+        title: `richest users in this server`,
+        description: pls.map((u, i) => `${emojis[i] || '👏'} ${u.coin.toLocaleString()} - ${u.username}#${u.discriminator}`).join('\n'),
+        footer: { text: `${msg.channel.guild.name} | add -all to see global` }
+      }
+    }
+  },
+  {
+    triggers: ['rich', 'richest', 'toponepercent'],
+    description: 'see who the top 10 richest users are in your server, or globally!'
+  }
+)
---
+++ src/commands/currencyCommands/steal.js
@@ -0,0 +1,90 @@
+const { GenericCommand } = require('../../models/')
+let min = 500
+
+const dmStolenUser = async (Memer, user, msg, worth) => {
+  if (!user.bot) {
+    try {
+      const channel = await Memer.bot.getDMChannel(user.id)
+      await channel.createMessage(`**${msg.author.username}#${msg.author.discriminator}** has stolen **${worth.toLocaleString()}** coins from you!`)
+    } catch (err) {
+      await msg.channel.createMessage(`${user.mention}, **${msg.author.username}#${msg.author.discriminator}** just stole **${worth.toLocaleString()}** coins from you!`)
+    }
+  }
+}
+
+module.exports = new GenericCommand(
+  async ({ Memer, msg, args, addCD }) => {
+    let user = msg.args.resolveUser(true)
+    if (!user) {
+      return 'try running the command again, but this time actually mention someone to steal from'
+    }
+    if (msg.author.id === user.id) {
+      return 'hey stupid, seems pretty dumb to steal from urself'
+    }
+    let perpCoins = await Memer.db.getCoins(msg.author.id)
+    let victimCoins = await Memer.db.getCoins(user.id)
+    let donor = await Memer.db.checkDonor(user.id)
+    if (perpCoins.coin < min) {
+      return `You need at least ${min} coins to try and rob someone.`
+    }
+    if (victimCoins.coin < min) {
+      return `The victim doesn't have at least ${min} coins, not worth it man`
+    }
+    if (donor < 5) { // $1-$4 gets 5% shields
+      victimCoins.coin = victimCoins.coin - (victimCoins.coin * 0.05)
+    } else if (donor < 10 && donor > 4) { // $5-$9 gets 25% shields
+      victimCoins.coin = victimCoins.coin - (victimCoins.coin * 0.25)
+    } else if (donor < 15 && donor > 9) { // $10-$14 gets 60% shields
+      victimCoins.coin = victimCoins.coin - (victimCoins.coin * 0.6)
+    } else if (donor < 20 && donor > 14) { // $15-$19 gets 80% shields
+      victimCoins.coin = victimCoins.coin - (victimCoins.coin * 0.8)
+    } else if (donor > 19) { // $20+ gets 95% shields
+      victimCoins.coin = victimCoins.coin - (victimCoins.coin * 0.95)
+    }
+    await addCD()
+    let stealingOdds = Math.floor(Math.random() * 100) + 1
+
+    if (stealingOdds <= 50) { // fail section
+      let punish
+      if ((perpCoins.coin * 0.05) < 500) {
+        punish = 500
+      } else {
+        punish = perpCoins.coin * 0.05
+      }
+      Memer.db.removeCoins(msg.author.id, Math.round(punish))
+      Memer.db.addCoins(user.id, Math.round(punish))
+      Memer.ddog.increment('stealFail')
+      return `You were caught! You paid the person you stole from **${Math.round(punish)}** coins.`
+    } else if (stealingOdds > 50 && stealingOdds <= 80) { // 30% payout
+      let worth = Math.round(victimCoins.coin * 0.3)
+      Memer.db.addCoins(msg.author.id, worth)
+      Memer.db.removeCoins(user.id, worth)
+      Memer.ddog.increment('stealSmall')
+      await dmStolenUser(Memer, user, msg, worth)
+      return `You managed to steal a small amount before leaving! 💸\nYour payout was **${worth.toLocaleString()}** coins.`
+    } else if (stealingOdds > 80 && stealingOdds <= 90) { // 50% payout
+      let worth = Math.round(victimCoins.coin * 0.5)
+      Memer.db.addCoins(msg.author.id, worth)
+      Memer.db.removeCoins(user.id, worth)
+      Memer.ddog.increment('stealLarge')
+      await dmStolenUser(Memer, user, msg, worth)
+      return `You managed to steal a large amount before leaving! 💰\nYour payout was **${worth.toLocaleString()}** coins.`
+    } else { // full theft up to 1 trillion
+      let worth = Math.round(victimCoins.coin)
+      Memer.db.addCoins(msg.author.id, worth)
+      Memer.db.removeCoins(user.id, worth)
+      Memer.ddog.increment('stealMAX')
+      await dmStolenUser(Memer, user, msg, worth)
+      return `You managed to steal a TON before leaving! 🤑\nYour payout was **${worth.toLocaleString()}** coins.`
+    }
+  },
+  {
+    triggers: ['steal', 'rob', 'ripoff'],
+    cooldown: 2e5,
+    donorCD: 1e5,
+    perms: ['embedLinks'],
+    description: 'Take your chances at stealing from users. Warning, you will lose money if you get caught!',
+    cooldownMessage: 'Woahhh there, you need some time to plan your next hit. Wait ',
+    missingArgs: 'You need to tag someone to steal from'
+  }
+)
